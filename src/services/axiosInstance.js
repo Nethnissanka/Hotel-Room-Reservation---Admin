b0991f3a9f98dc5b69f717d385738cb75055ba2c
N.A.N.N.N. Arachchi <158262381+Nethnissanka@users.noreply.github.com>
2024-10-13 00:59:37 +0530

    Update axiosInstance.js

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with a default base URL
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Adjust to your backend's address
+  baseURL: 'https://hotel-room-reservation-backend.onrender.com', // Adjust to your backend's address
 });
 
 // Optionally, you can add interceptors for request/response
